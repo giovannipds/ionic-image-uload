7e770d5e7eb94b078bb2c3d4dbec05f4bc20074e
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-23 10:44:21 -0200

    Import dependencies at home controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ImagePicker } from '@ionic-native/image-picker';
+import { Base64 } from '@ionic-native/base64';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
