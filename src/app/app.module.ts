593cac81b4b88a04ded858b04476829e265e57df
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-23 10:42:17 -0200

    Import/register imagepicker/base64 plugin at app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { ImagePicker } from '@ionic-native/image-picker';
+import { Base64 } from '@ionic-native/base64';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +27,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ImagePicker,
+    Base64,
   ]
 })
 export class AppModule {}
