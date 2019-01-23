bb9017b60143b2efcc24ec541ba39a2c280fbd9a
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-23 11:05:18 -0200

    Import dependencies at detail page

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /**
  * Generated class for the DetailPage page.
